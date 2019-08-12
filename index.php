9d4116fbc48decbc59186265ecaf5654c4ae96ee
Misou <micka75bup@gmail.com>
2019-08-12 17:11:42 +0200

    Appel dynamique des méthodes Controller

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 require "vendor/autoload.php";
 
 $router = new Control\Router($_GET["url"]);
-$ctrlHome = new Control\ControllerHomepage();
 
-
-$router->getRoutes("accueil", $ctrlHome->homepage());
+$router->getRoutes("accueil", "ControllerHomepage+homepage");
+$router->getRoutes("blog", "ControllerBlog+blog");
+$router->getRoutes("about", "ControllerAbout+about");
+$router->getRoutes("contact", "ControllerContact+contact");
 $router->run();
+
