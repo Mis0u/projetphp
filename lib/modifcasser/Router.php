5418023bab2511279dcc4504b4f1b536814bea80
Misou <micka75bup@gmail.com>
2019-10-28 21:56:32 +0100

    modif cass

---
+++ lib/modifcasser/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lib\router;
+
+class Router
+{
+    private $url ;
+    private $routes = [];
+    private $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRoutes($path, $callable)
+    {
+        $route = new Path($path, $callable);
+        $this->routes[] = $route;
+    }
+
+    public function run()
+    {
+        foreach ($this->routes as $route):
+            if ($route->match($this->request->getUri())){
+               return $route->call($this->request);
+            }
+        endforeach;    
+        throw new \Exception("URL non valide");
+    }   
+
+}
